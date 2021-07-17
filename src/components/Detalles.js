7a079f52f156aefefd4a8de78e52f79efbc4d562
Luciano <lucianoojeda36@gmail.com>
2021-07-17 18:20:38 -0300

    actualizacion

--- src/components/Detalles.js
+++ src/components/Detalles.js
@@ -40,77 +40,98 @@ const useStyles = makeStyles({
 function monthDosCifras() {
     let month = new Date().getMonth()
     return month < 10 ? '0' + `${month + 1}` : month
+
+}
+
+const elementos = [1,2,4,5,6,10,50,12,24,25,100]
+
+function handelFilter (e) {
+    for (var i = 0; i < elementos.length; i++) {
+        if (e == elementos[i]) {
+            return elementos[i]
+        }
+    }
 }
 
 export default function Detalles(props) {
     const classes = useStyles();
 
 
-    console.log("que es esto",props)
+    console.log("que es esto", props)
 
 
-    function precio_unitario (row){
-        const precio_init=row.__EMPTY_1
-        const match1= row.__EMPTY ? row.__EMPTY.match(/(\d+)/g) : 1
-        const match_unidad= parseFloat(match1)
-        const precio_1 = (precio_init)/(match_unidad=== 5 ||1||10||100 ? match_unidad : 1)
-        const precio_iva = (precio_1 + (0.21*precio_1))
-        const precio_final= parseFloat(`${precio_iva + ((props.porcentaje/100)*precio_iva)}`).toFixed(2)
+    function precio_unitario(row) {
 
-        
+        const precio_init = row.__EMPTY_1
+        const match1 = row.__EMPTY.match(/(\d+)/g).filter((e) => e==handelFilter(e)).length > 0 ? row.__EMPTY.match(/(\d+)/g).filter((e) => e==handelFilter(e)) : ['1']
+        const match_unidad = parseFloat(match1)
+        const precio_1 = (precio_init) / (match_unidad === 5 || 6 || 12 || 1 || 10 || 100 ? match_unidad : 1)
+        const precio_iva = (precio_1 + (0.21 * precio_1))
+        const precio_final = parseFloat(`${precio_iva + ((props.porcentaje / 100) * precio_iva)}`).toFixed(2)
+
+        // console.log(precio_init, "precio_int")
+        // console.log(match1, "match1")
+        // console.log(row.__EMPTY.match(/(\d+)/g).filter((e) => e == handelFilter(e)), "probando")
+        // console.log(row.__EMPTY.match(/(\d+)/g), "con coma")
+        // console.log(match_unidad, "match_unidad")
+        // console.log(precio_1, "precio_1")
+        // console.log(precio_iva, "precio_iva")
+        // console.log(precio_final, "precio_final")
+
+
         return precio_final
     }
 
-    
-    function precio_bulto (row){
+
+    function precio_bulto(row) {
         const precio_1 = row.__EMPTY_1
-        const precio_iva = (precio_1 + (0.21*precio_1))
-        const precio_final= parseFloat(`${precio_iva + ((props.porcentaje/100)*precio_iva)}`).toFixed(2)
-        
+        const precio_iva = (precio_1 + (0.21 * precio_1))
+        const precio_final = parseFloat(`${precio_iva + ((props.porcentaje / 100) * precio_iva)}`).toFixed(2)
+
         return precio_final
     }
 
 
-    if (props.Results_Array){
+    if (props.Results_Array) {
 
         return (
-    
-            <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Codigo</StyledTableCell>
-                        <StyledTableCell align="right">Nombre Producto</StyledTableCell>
-                        <StyledTableCell align="right">Precio por Bulto</StyledTableCell>
-                        <StyledTableCell align="right">Precio Unitario</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.Results_Array.map((row) => (
-                        <StyledTableRow key={row[`PRECIOS AL ${new Date().getDate()}/${monthDosCifras()}/${new Date().getFullYear()}`]}>
-                            <StyledTableCell component="th" scope="row">
-                                {row[`PRECIOS AL ${new Date().getDate()}/${monthDosCifras()}/${new Date().getFullYear()}`]}
-                            </StyledTableCell>
-                            <StyledTableCell align="right">{row.__EMPTY}</StyledTableCell>
-                            <StyledTableCell align="right">{precio_bulto(row)}</StyledTableCell>
-                            <StyledTableCell align="right">{precio_unitario(row)}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer> 
-        
-    )
 
-    }else {
-        return(
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Codigo</StyledTableCell>
+                            <StyledTableCell align="right">Nombre Producto</StyledTableCell>
+                            <StyledTableCell align="right">Precio por Bulto</StyledTableCell>
+                            <StyledTableCell align="right">Precio Unitario</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.Results_Array.map((row) => (
+                            <StyledTableRow key={row[`PRECIOS AL ${new Date().getDate()}/${monthDosCifras()}/${new Date().getFullYear()}`]}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row[`PRECIOS AL ${new Date().getDate()}/${monthDosCifras()}/${new Date().getFullYear()}`]}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.__EMPTY}</StyledTableCell>
+                                <StyledTableCell align="right">{precio_bulto(row)}</StyledTableCell>
+                                <StyledTableCell align="right">{precio_unitario(row)}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+        )
+
+    } else {
+        return (
             <div>
                 <h1>
-                Todavia no cargaste el Archivo
+                    Todavia no cargaste el Archivo
                 </h1>
             </div>
         )
-        
+
     }
-    
+
 }
